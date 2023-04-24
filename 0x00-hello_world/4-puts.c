07295ccad6f60149baa0628b909499a1a64b570f
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-04-24 11:48:42 -0700

    my 4th task yet again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - this function will print the statement in Mystr
+ */
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
+
 	return(0);
 }
