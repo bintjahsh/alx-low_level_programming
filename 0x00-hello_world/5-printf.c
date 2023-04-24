c4715576b67329e22764321bb542eee979f59c35
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-04-24 14:34:16 -0700

    my 5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * printf - will print out the statement
+ *
  * return: 0 if the printf is succesful
  */
 int main(void)
 {
-	printf ("%s, \n", "with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
