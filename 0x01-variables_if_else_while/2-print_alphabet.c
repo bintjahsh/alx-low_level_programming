f650230509847e2302cb10a497c837ae8d04a3ad
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-04-24 20:44:35 -0700

    my 2nd task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 if main is successful
+ */
+int main (void)
+{
+	char start = 'a';
+	char end = 'z';
+	char ch = start;
+	while(ch != end)
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	return (0);
+}
