eb976354d8e8525e7ebc6ee5a05822491e378fef
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-04-25 20:44:03 -0700

    my 5th task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * orint_sign - checks the sign of a number
+ *
+ * Return: 1 if positive. 0 if equal to 0 and -1 if negative
+ */
+int print_sign(int num)
+{
+	if (num > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
