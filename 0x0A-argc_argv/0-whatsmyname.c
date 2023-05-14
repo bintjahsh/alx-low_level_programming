92968fd00cac426f4e6e0dec5893b913e59da3c7
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-05-14 12:46:34 -0700

    my zeroth task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * return: 0 if main is succesful
+ */
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+My journey to argv and argc
