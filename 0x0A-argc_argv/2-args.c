56f5f7e092c50b03a08281fb82ca3051f7d0b32a
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-05-16 23:16:23 -0700

    my 2nd and 3rd tasks again o

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments passed to it.
- * @argc: the number of arguments passed to it.
- * @argv: an array of pointer to all the arguments.
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * return: 0 if main is succesful.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
 
-		return (0);
-	}
+	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
- * @argc: number of arguments supplied to main.
- * @argv: an array of pointers to arguments supplied to main.
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * return: 0 if main is successful.
- * 	   1 if main receives less than two arguments.
+ * Return: If the program receives two arguments - 0.
+ *         If the program does not receive two arguments - 1.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	int num1;
+	int num2; 
+	int mul;
 
 	if (argc != 3)
 	{
@@ -19,7 +21,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = atoi(argv[1];
+	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	mul = num1 * num2;
 
