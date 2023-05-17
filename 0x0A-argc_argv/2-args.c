66c2fa971a50dd1e7ba23d60b6a9f5945beb9603
Zainab Olurode-Razaq <bintjash@gmail.com>
2023-05-16 23:03:22 -0700

    my 2nd & 3rd task again

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints al arguments passed to it.
+ * main - prints all arguments passed to it.
  * @argc: the number of arguments passed to it.
  * @argv: an array of pointer to all the arguments.
  *
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0, i < argc, i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,29 +4,26 @@
 /**
  * main - multiplies two numbers.
  * @argc: number of arguments supplied to main.
- * @argv: an array of arguments supplied to main.
+ * @argv: an array of pointers to arguments supplied to main.
  *
  * return: 0 if main is successful.
- * 	1 if main receives less
- * 	   than two arguments.
+ * 	   1 if main receives less than two arguments.
  */
 int main(int argc, char *argv[])
 {
 	int num1, num2, mul;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num1 = atoi(argv[1];
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
 
-		printf("%d\n", mul);
+	num1 = atoi(argv[1];
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
 
-		return (0);
-	}
+	printf("%d\n", mul);
+
+	return (0);
 }
